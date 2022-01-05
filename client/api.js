e2633f2e3751a4af09b5f290134a55ed31665975
deanblinder <62507744+deanblinder@users.noreply.github.com>
2022-01-05 18:50:11 +0200

    first commit

--- client/api.js
+++ client/api.js
@@ -8,14 +8,14 @@ const createApiClient = () => {
         changeAgentParams:(agent,latency,variance)=>{
             return axios.post(`http://172.20.10.3:3232/api/changeAgentParams/${agent}/${latency}/${variance}`).then((res) => res.data);
         },
-        registerPlayer:(mail,age,gender)=>{
-            return axios.post(`http://172.20.10.3:3232/api/registerPlayer/${mail}/${age}/${gender}`).then((res) => res.data);
+        registerPlayer:(mail,age,gender,deviceUid)=>{
+            return axios.post(`http://172.20.10.3:3232/api/registerPlayer/${mail}/${age}/${gender}/${deviceUid}`).then((res) => res.data);
         },
-        sendQuestionnaireAnswers:(qDict)=>{
-            return axios.post(`http://172.20.10.3:3232/api/questionnaireAnswers`,qDict).then((res) => res.data);
+        sendQuestionnaireAnswers:(qDict,deviceUid)=>{
+            return axios.post(`http://172.20.10.3:3232/api/questionnaireAnswers/${deviceUid}`,qDict).then((res) => res.data);
         },
-        sendFeedBack:(feedBack)=>{
-            return axios.post(`http://172.20.10.3:3232/api/sendFeedBack/${feedBack}`).then((res) => res.data);
+        sendFeedBack:(feedBack,deviceUUID)=>{
+            return axios.post(`http://172.20.10.3:3232/api/sendFeedBack/${feedBack}/${deviceUUID}`).then((res) => res.data);
         },
         sendPressTimeStamp:(playerData, agentData)=>{
             console.log(playerData, agentData)
